59697db0356f9f6dd121b4e2648e74a8ddb3181a
Mariana <mariana.balanchuk@gmail.com>
2023-02-19 14:29:04 +0100

    moved code from codesandbox

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import "./App.css";
-import Weather from "./Weather";
+import SearchForm from "./SearchForm/SearchForm";
+import CurrentData from "./CurrentData/CurrentData";
+import AuthorInfo from "./AuthorInfo/AuthorInfo";
 
-function App() {
-  return <Weather city="Poznan" />;
+// function handleResponse(response) {
+//   alert(`The weather in ${props.city} is ${response.data.main.temp}°C`);
+// }
+// let apiKey = "7a00a4fb22b18bae5dbea39280ad220a";
+// let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+// axios.get(apiUrl).then(handleResponse);
+
+function App(props) {
+  return (
+    <div className="container">
+      <SearchForm />
+      <CurrentData />
+      <AuthorInfo />
+    </div>
+  );
 }
 
 export default App;
