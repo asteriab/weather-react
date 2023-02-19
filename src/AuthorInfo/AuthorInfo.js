a81decee95b499f96f5e686c28a7125cdf157f9f
Mariana <mariana.balanchuk@gmail.com>
2023-02-19 21:25:46 +0100

    updated link to github

--- src/AuthorInfo/AuthorInfo.js
+++ src/AuthorInfo/AuthorInfo.js
@@ -3,7 +3,10 @@ import React from "react";
 export default function AuthorInfo() {
   return (
     <p className="author-info">
-      <a href="https://github.com/asteriab/weather-app" className="github-link">
+      <a
+        href="https://github.com/asteriab/weather-react"
+        className="github-link"
+      >
         Open-source code
       </a>
       by Mariana Balanchuk
